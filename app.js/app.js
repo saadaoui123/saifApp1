d9932f66237b3f9548c1754c4aebad2f144ac436
saadaoui123 <160061833+saadaoui123@users.noreply.github.com>
2024-02-14 20:29:57 +0100

    Add files via upload

---
+++ app.js/app.js
@@ -0,0 +1,48 @@
+// EX1
+/*
+function max(a,b){
+    if (a>b)
+    return a
+    else 
+    return b
+
+}
+
+console.log(max(3,5))
+*/
+
+// EX2
+/*
+function aff(char){
+    return char
+}
+
+console.log(aff("bonjour"))
+*/
+//EX3
+/*
+function somme(a,b){
+    a=prompt("Donner le premier nombre")
+    b=prompt("Donner la deuxieme nombre")
+    alert(Number(a)+Number(b))
+}
+
+console.log(somme(3,7))
+*/
+
+//EX4
+/*
+const container = document.getElementById("zone")
+
+function into(){
+    container.innerHTML = "Enter"
+}
+
+function out(){
+    container.innerHTML = "Out"
+}
+
+function bg(){
+    container.style.backgroundColor = "red"
+}
+*/
